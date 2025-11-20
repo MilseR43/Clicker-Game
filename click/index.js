d9ff98b23a1846af6f9e581311f1d4770df02490
MilseR43 <miles.rueckheim29@student.tamdistrict.org>
2025-11-20 05:33:27 +0000

    Finished click thingy

--- click/index.js
+++ click/index.js
@@ -1,15 +1,59 @@
 const clickButton=document.getElementById("clickButton");
 const clickCountDisplay=document.getElementById("clickCountDisplay");
-const Upgrade1=document.getElementById("Upgrade1")
-let clickCount = 0; 
+const Upgrade1=document.getElementById("Upgrade1");
+const SlotMachine=document.getElementById("SlotMachine");
+const ClickSfx=document.getElementById("ClickSfx");
+const SlotMachine2=document.getElementById("SlotMachine2");
+const ekhSfx=document.getElementById("ekhSfx");
 
+let clickCount = 0;
+let costOfUpgrade1= 10;
+let multiplier=1;
+let upgradeCost=2;
+let costOfSlotMachine=100;
+
+function clicksDisplaied(){
+    clickCountDisplay.innerText="you have clicked the button: "+ clickCount + " times!"
+};
 clickButton.addEventListener("click", ()=>{
-    clickCount+=1;
-    clickCountDisplay.innerText=clickCount;
+    clickCount+=multiplier
+    clicksDisplaied();
+    ekhSfx.play();
+ekhSfx.currentTime=0;
 });
 
-Upgrade1.addEventListener("click,", ()=>{
-if (clickCountDisplay >=10){
-clickCountDisplay.innerText=clickCount-10;
+Upgrade1.addEventListener("click", ()=>{
+if (clickCount >= costOfUpgrade1){
+    clickCount-= costOfUpgrade1;
+    multiplier++;
+    costOfUpgrade1*=2;
+    clicksDisplaied();
+    Upgrade1.innerText= "upgrade for "+ costOfUpgrade1 + " clicks"
+}
+else{
+    clickCountDisplay.innerText="not enough clicks."
+}
+});
+SlotMachine.addEventListener("click", ()=>{
+if(clickCount >= costOfSlotMachine){
+    clickCount-= costOfSlotMachine;
+    let chance = Math.random();
+    if (chance < 0.1)
+        SlotMachine.innerText="you Win common prize"
+        else{
+        SlotMachine.innerText="you Lose"
+    }
+}
+});
+SlotMachine2.addEventListener("input", ()=>{
+if(clickCount >= costOfSlotMachine){
+    clickCount-= costOfSlotMachine;
+    let chance = Math.random();
+    if (chance < 0.01)
+        SlotMachine.innerText="you Win rare prize";
+    else{
+        SlotMachine.innerText="you Lose";
+        let aud
+    }
 }
 });
